5d2566147b381cfc63202477846c8123c99110d7
Sathish Velayudam <sathish.velayudam@manheim.com>
2018-02-16 23:13:35 -0500

    Exercises

---
+++ ex10.rb
@@ -0,0 +1,23 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+fat_cat2 = '''
+It\'s a fatty
+\ttabby
+cat
+'''
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+puts fat_cat2
+
---
+++ ex11.rb
@@ -0,0 +1,9 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp.to_i
+print "How much do you weight? "
+weight = gets.chomp.to_f
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
